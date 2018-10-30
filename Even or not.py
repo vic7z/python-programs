efc6fcf67ff4ae1c799d32c32ffd6b040241b91a
victorgpz <touchwithvictor@gmail.com>
2018-10-30 22:40:14 +0530

    py2

---
+++ Even or not.py
@@ -0,0 +1,5 @@
+n =int(input("enter limit")) 	#input
+if (n%2==0):
+	print("even no")
+else:
+	print("not a even no")
---
+++ EvennoUptoNnumber.py
@@ -0,0 +1,4 @@
+n =int(input("enter limit")) 	#input
+for i in range(n):   					#for loop
+	if(i%2==0):     					#even or not checking
+		print(i)
