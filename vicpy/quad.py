b358e0487fde9471ff9048476b7daa389f6e3770
Victor <35284183+Victorgpz@users.noreply.github.com>
2018-11-21 11:44:26 +0530

    Add files via upload
    py2

--- vicpy/quad.py
+++ vicpy/quad.py
@@ -1,1 +1,9 @@
-
+import math
+a=int(input("a"))
+b=int(input("b"))
+c=int(input("c"))
+s1,s2=0,0
+s1=-b+math.sqrt((b**2)-4*a*c)
+print(s1/2*a)
+s2=-b-math.sqrt((b**2)-4*a*c)
+print(s2/2*a)
--- vicpy/vowelinastring.py
+++ vicpy/vowelinastring.py
@@ -2,12 +2,7 @@ def vol(a):
 	an=["a","e","i","o","u","A","E","I","O"]
 	for s in a:
 		if s in an:
-			return 1
-		else:
-			return 0
+			print(s)
 
-a=raw_input("enter a string  \t:")
-if (vol(a)):
-	print("the string contain vowel")
-else:
-	print("the string does not contain vowel")
+q=raw_input("enter a string  \t:")
+vol(q)
